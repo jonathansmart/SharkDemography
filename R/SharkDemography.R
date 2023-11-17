b54e39727f21f3858d8b61328eab651ffd5f30e1
Jonathan Smart <Jonathan.Smart@daf.qld.gov.au>
2023-11-17 13:33:36 +1000

    Silky data added

--- R/SharkDemography.R
+++ R/SharkDemography.R
@@ -11,3 +11,14 @@ utils::globalVariables(c(
   'rnorm','runif','txtProgressBar','setTxtProgressBar','quantile','Method','Value','parse_number','info','F.','mean.lambda',
   'AVG','.','Lambda','MaxAge', 'MinAge', 'Range', 'na.omit','.data'
 ))
+
+#' Example life history data for Silky Sharks
+#'
+#' An example dataset of a completed set of life history data for Silky Sharks
+#' in a `create_data_input()` template.
+#' @docType data
+#' @keywords datasets
+#' @name Silky_data
+#' @usage data(Silky_data)
+#' @format A list of 10 with class "Demography.inputs"
+NULL
